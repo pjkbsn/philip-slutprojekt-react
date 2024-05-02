5a512f21d216e310b6ace01cc4b1e71b4976cfc8
pjkbsn <philip.jkbsn@gmail.com>
2024-05-02 19:16:08 +0200

    added types

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type BookType = {
+  title: string;
+  author_name: string;
+  first_publish_year: number;
+  first_sentence: string;
+};
+
+// export type MonsterContextType = {
+//   currentBook: BookType[];
+//   dispatch: React.Dispatch<MonsterAction>;
+// };
+
+// export type MonsterAction =
+//   | { type: "VIEW_BOOKS"; payload: BookType }
+//   | { type: "ADD_BOOKS"; payload: string };
