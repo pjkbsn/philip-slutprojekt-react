5a909bcc6983cc8b89d0dd6a8ea065092c5fa57e
pjkbsn <philip.jkbsn@gmail.com>
2024-05-10 10:32:03 +0200

    Added rootpage

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../../components/NavBar/NavBar";
 import "./Root.scss";
-// import { RatingComponent } from "../../components/Rating";
-// import { Books } from "../../data/fetchAPI";
 
 export const Root = () => {
   return (
     <div className="Root">
       <NavBar />
-      {/* <RatingComponent /> */}
-      {/* <Books /> */}
+
       <Outlet />
     </div>
   );
