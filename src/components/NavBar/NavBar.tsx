5c717008c69dd72559374f4b46be8a77210898ea
pjkbsn <philip.jkbsn@gmail.com>
2024-05-10 10:28:57 +0200

    Added a navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,32 @@
 import "./NavBar.scss";
 import { SearchField } from "../SearchField/SearchField";
+import { NavLink } from "react-router-dom";
+// import { MyListbox } from "../ListBox/ListBox";
 
 export const NavBar = () => {
   return (
     <>
       <div className="NavBar">
-        <div>Search Bar</div>
-        <SearchField />
+        <div className="leftsideNavbar">
+          <NavLink to="/" className="navBarLink">
+            Home
+          </NavLink>
+          {/* <div className="listBox">
+            <MyListbox />
+          </div> */}
+          <SearchField />
+        </div>
+        <div className="rightsideNavbar">
+          <NavLink to="/favorites/" className="navBarLink">
+            Favorites
+          </NavLink>
+          <NavLink to="/read/" className="navBarLink">
+            Read
+          </NavLink>
+          <NavLink to="/categories" className="navBarLink">
+            Categories
+          </NavLink>
+        </div>
       </div>
     </>
   );
