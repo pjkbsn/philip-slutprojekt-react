cf8d36b49867dfbd622cdb118bd35521487f8e1c
pjkbsn <philip.jkbsn@gmail.com>
2024-05-10 10:26:16 +0200

    Added route and function to display books

--- src/components/DisplayBooks/DisplayBooks.tsx
+++ src/components/DisplayBooks/DisplayBooks.tsx
@@ -1,44 +1,59 @@
-import { useContext } from "react";
+import { DisplayBooksType } from "../../types";
 import "./DisplayBooks.scss";
-import {
-  BookContext,
-  BookContextType,
-} from "../../data/BookProvider/BookProvider";
 
-export const DisplayBooks = () => {
-  const { data, loading, error } = useContext(BookContext) as BookContextType;
-  console.log(data);
+// import { useContext } from "react";
+// import { BookContext } from "../../data/BookProvider/BookProvider";
+// import { BookContextType } from "../../types";
 
-  if (loading) {
-    return <p>..Loading</p>;
-  }
-  if (error) {
-    return <p>Error: {error.toString()}</p>;
-  }
-  if (data) {
-    return (
-      <ul>
-        {data.map((book, index) => (
-          <li key={index}>
-            <h2>{book.title}</h2>
-            <p>Author: {book.author_name}</p>
-            {book.cover_i ? (
-              <img
-                src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
-                alt="Book Cover"
-              />
-            ) : (
-              <p>No cover available</p>
-            )}
-          </li>
-        ))}
-      </ul>
-    );
-  }
+import { NavLink } from "react-router-dom";
 
-  // return (
-  //   <div className="DisplayBooks">
-  //     <h1>DisplayBooks</h1>
-  //   </div>
-  // );
+export const DisplayBooks = ({ data }: DisplayBooksType) => {
+  // const { data, loading, error } = useContext(BookContext) as BookContextType;
+  // console.log(data);
+
+  // if (loading) {
+  //   return (
+  //     <div className="DisplayBooks">
+  //       <p>..Loading</p>
+  //     </div>
+  //   );
+  // }
+  // if (error) {
+  //   return (
+  //     <div className="DisplayBooks">
+  //       <p>Error: {error.toString()}</p>
+  //     </div>
+  //   );
+  // }
+  // if (data) {
+
+  return (
+    <div className="DisplayBooks">
+      <div className="displayContainer">
+        {data &&
+          data.map((book, index) => (
+            <div key={index} className="bookInfoContainer">
+              {book.cover_i ? (
+                <img
+                  className="bookCover"
+                  src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
+                  alt="Book Cover"
+                />
+              ) : (
+                <p>No cover available</p>
+              )}
+              <div className="titleContainer">
+                <h3>{book.title}</h3>
+                <p>{book.author_name}</p>
+              </div>
+              {/* <NavLink to={`/book${book.key}`}>Details</NavLink> */}
+              <NavLink to={`/book/${encodeURIComponent(book.key)}`}>
+                Details
+              </NavLink>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+  // }
 };
--- src/routes/DisplayedBooks/DisplayedBooks.tsx
+++ src/routes/DisplayedBooks/DisplayedBooks.tsx
@@ -1,10 +1,33 @@
+import { useContext } from "react";
 import { DisplayBooks } from "../../components/DisplayBooks/DisplayBooks";
+import { BookContext } from "../../data/BookProvider/BookProvider";
+import { BookContextType } from "../../types";
 import "./DisplayedBooks.scss";
 
 export const DisplayedBooks = () => {
-  return (
-    <div className="DisplayedBooks">
-      <DisplayBooks />
-    </div>
-  );
+  const { data, loading, error } = useContext(BookContext) as BookContextType;
+
+  console.log(data);
+
+  if (loading) {
+    return (
+      <div className="DisplayBooks">
+        <p>..Loading</p>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="DisplayBooks">
+        <p>Error: {error.toString()}</p>
+      </div>
+    );
+  }
+  if (data) {
+    return (
+      <>
+        <DisplayBooks data={data} />
+      </>
+    );
+  }
 };
