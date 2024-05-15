d9dc8649abaa2b48d827ca1c128ecffb542c34bf
pjkbsn <philip.jkbsn@gmail.com>
2024-05-15 13:39:28 +0200

    Added readfunctions

--- src/components/Read/Read.tsx
+++ src/components/Read/Read.tsx
@@ -1,7 +1,20 @@
+import { useContext } from "react";
+import { BookContext } from "../../data/BookProvider/BookProvider";
+import { BookContextType } from "../../types";
+import { DisplayBooks } from "../DisplayBooks/DisplayBooks";
+
 export const Read = () => {
+  const { readList } = useContext(BookContext) as BookContextType;
+  const totalBooksRead = readList.length;
+  const totalPages = readList.reduce(
+    (accumulator, currentValue) => accumulator + (currentValue.pages || 0),
+    0
+  );
   return (
     <>
-      <h1>Read</h1>
+      <h1>Books read: {totalBooksRead}</h1>
+      <h1>Total pages: {totalPages}</h1>
+      {readList && <DisplayBooks data={readList} />}
     </>
   );
 };
---
+++ src/routes/ReadPage/ReadPage.tsx
@@ -0,0 +1,9 @@
+import { Read } from "../../components/Read/Read";
+
+export const ReadPage = () => {
+  return (
+    <>
+      <Read />
+    </>
+  );
+};
